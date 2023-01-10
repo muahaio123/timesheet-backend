9fdad9bd0498123953ab629490a98260ebaada1b
saffdas <50192880+saffdas@users.noreply.github.com>
2023-01-10 14:39:38 -0500

    -addding crontab for each Sat add a new timesheet detail and one line in timesummary

--- timesheet-summary-service/src/main/java/com/beaconfire/timesheetsummaryservice/controller/TimesheetController.java
+++ timesheet-summary-service/src/main/java/com/beaconfire/timesheetsummaryservice/controller/TimesheetController.java
@@ -2,9 +2,12 @@ package com.beaconfire.timesheetsummaryservice.controller;
 
 import com.beaconfire.timesheetsummaryservice.domain.ServiceStatus;
 import com.beaconfire.timesheetsummaryservice.domain.entity.TimesheetDetail;
+import com.beaconfire.timesheetsummaryservice.domain.entity.TimesheetStatus;
 import com.beaconfire.timesheetsummaryservice.domain.entity.TimesheetSummary;
+import com.beaconfire.timesheetsummaryservice.domain.request.SubmissionStatus;
 import com.beaconfire.timesheetsummaryservice.domain.response.MessageResponse;
 import com.beaconfire.timesheetsummaryservice.service.TimesheetService;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +36,15 @@ public class TimesheetController {
       return new TimesheetSummary();
     }
   }
+  @GetMapping("/WeekEndings/{id}")
+  public List<TimesheetStatus> getTimesheetSummaryStatusListById(@PathVariable String id) {
+    if (timesheetService.findTimesheetSummaryByEmployeeId(id).isPresent()){
+      return timesheetService.findTimesheetSummaryByEmployeeId(id).get().getTimesheetStatusList();
+    }
+    else{
+      return new ArrayList<>();
+    }
+  }
   @PostMapping("/save")
   public MessageResponse saveTimesheetSummary(@RequestBody TimesheetSummary timesheetSummary){
     timesheetService.saveTimesheetSummary(timesheetSummary);
@@ -44,6 +56,42 @@ public class TimesheetController {
         )
         .message("New TimeSheet summary saved")
         .build();
+  }
+  @PostMapping("/edit")
+  public MessageResponse editTimesheetSummarySubmissionStatus(@RequestBody SubmissionStatus submissionStatus){
+    //TODO change the hardcode employeeId;
+    String employeeId="1";
+    String weekEnding=submissionStatus.getWeekEnding();
+    Optional<TimesheetSummary> timesheetSummary=timesheetService.findTimesheetSummaryByEmployeeId(employeeId);
+    if(timesheetSummary.isPresent()){
+      for(TimesheetStatus timesheetStatus:timesheetSummary.get().getTimesheetStatusList()){
+        if(timesheetStatus.getWeekEnding().equals(weekEnding)){
+          timesheetStatus.setApprovalStatus(submissionStatus.getApprovalStatus());
+          timesheetSummary.get().setTimesheetStatusList(timesheetSummary.get().getTimesheetStatusList());
+          break;
+        }
+      }
+      timesheetService.saveTimesheetSummary(timesheetSummary.get());
+
+      return MessageResponse.builder()
+          .serviceStatus(
+              ServiceStatus.builder()
+                  .success(true)
+                  .build()
+          )
+          .message("Successfully "+submissionStatus.getApprovalStatus())
+          .build();
+    }
+    else{
+      return MessageResponse.builder()
+          .serviceStatus(
+              ServiceStatus.builder()
+                  .success(true)
+                  .build()
+          )
+          .message("not found data")
+          .build();
+    }
   }
   @DeleteMapping("/delete/{id}")
   public MessageResponse deleteTimesheetSummary(@PathVariable String id){
---
+++ timesheet-summary-service/src/main/java/com/beaconfire/timesheetsummaryservice/domain/request/SubmissionStatus.java
@@ -0,0 +1,11 @@
+package com.beaconfire.timesheetsummaryservice.domain.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SubmissionStatus {
+  String weekEnding;
+  String approvalStatus;
+}

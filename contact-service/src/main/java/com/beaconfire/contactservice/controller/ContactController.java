5816c8f9853fb879011c9aed619303057045d1fd
ht--j <huantong6652@gmail.com>
2023-01-10 08:57:57 -0800

    second add

--- contact-service/src/main/java/com/beaconfire/contactservice/controller/ContactController.java
+++ contact-service/src/main/java/com/beaconfire/contactservice/controller/ContactController.java
@@ -5,10 +5,8 @@ import com.beaconfire.contactservice.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.rmi.ServerException;
 import java.sql.SQLException;
 import java.util.List;
 

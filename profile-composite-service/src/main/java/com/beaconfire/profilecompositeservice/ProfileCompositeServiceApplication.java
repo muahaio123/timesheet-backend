a45d16a0a5b051584f371604a2ab8d4ce4ad8ab0
Richard <r1ch4rdph0en1x@gmail.com>
2023-01-11 02:41:35 -0800

    config profile&timesheet composite services

--- profile-composite-service/src/main/java/com/beaconfire/profilecompositeservice/ProfileCompositeServiceApplication.java
+++ profile-composite-service/src/main/java/com/beaconfire/profilecompositeservice/ProfileCompositeServiceApplication.java
@@ -2,8 +2,10 @@ package com.beaconfire.profilecompositeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProfileCompositeServiceApplication {
 
 	public static void main(String[] args) {
--- timesheet-composite-service/src/main/java/com/beaconfire/timesheetcompositeservice/TimesheetCompositeServiceApplication.java
+++ timesheet-composite-service/src/main/java/com/beaconfire/timesheetcompositeservice/TimesheetCompositeServiceApplication.java
@@ -2,8 +2,10 @@ package com.beaconfire.timesheetcompositeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TimesheetCompositeServiceApplication {
 
 	public static void main(String[] args) {
